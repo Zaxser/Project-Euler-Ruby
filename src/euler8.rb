c176c7f5718496b02fbe880799f66380caaced9b
Zaxser <nerlenemaedavis@gmail.com>
2021-06-17 12:58:03 +0800

    Solved the 8th Euler problem; parsing is less than elegant; will have to look at it again later.

--- src/euler8.rb
+++ src/euler8.rb
@@ -26,53 +26,70 @@
 
 # Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-input = open("../input/euler8.txt").readlines()
-rows = input.map do |r| 
-  r.split("").map {|s| s.to_i}
-end
+# input = open("../input/euler8.txt").readlines().join("").split("").map {|i| i.to_i}
 
-def rows_to_columns(rows)
-  rows.map.with_index do |_, index| 
-    rows.map {|r| r[index]}
+# Hmm. There was some problem trying to parse then into a proper number before.
+# Will have to fix later.
+
+input = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+input = input.split("").map {|i| i.to_i}
+
+def max_consectutive_product(input, sequence_length)
+  max = 0
+  max_sequence = []
+  input.each_cons(sequence_length) do |sequence|
+    product = sequence.inject(:*)
+    max = product if product > max
   end
+  max
 end
 
-test = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]]
+p max_consectutive_product(input, 4)
+p max_consectutive_product(input, 13)
 
-p test
-p rows_to_columns(test)
+# def rows_to_columns(rows)
+#   rows.map.with_index do |_, index| 
+#     rows.map {|r| r[index]}
+#   end
+# end
 
-def rows_to_right_diagonals(rows)
-  limit = rows.length
-  diagonals = []
-  ((limit * 2) - 1).times do |sum|
-    diagonal = []
-    (sum + 1).times do |y|
-      x = sum - y
-     if rows[y] and rows[y][x]
-      diagonal << rows[y][x]  
-     end
-    end
-    diagonals << diagonal
-  end
-  return diagonals
-end
+# test = [
+#   [1, 2, 3],
+#   [4, 5, 6],
+#   [7, 8, 9]]
 
-def rows_to_left_diagonals(rows)
-  rows = rows.map do |row|
-    row.reverse()
-  end
-  rows_to_right_diagonals(rows)
-end
+# p test
+# p rows_to_columns(test)
 
-p rows_to_right_diagonals(test)
-p rows_to_left_diagonals(test)
+# def rows_to_right_diagonals(rows)
+#   limit = rows.length
+#   diagonals = []
+#   ((limit * 2) - 1).times do |sum|
+#     diagonal = []
+#     (sum + 1).times do |y|
+#       x = sum - y
+#      if rows[y] and rows[y][x]
+#       diagonal << rows[y][x]  
+#      end
+#     end
+#     diagonals << diagonal
+#   end
+#   return diagonals
+# end
 
-def max_product(rows, sequence_length):
-  columns = rows_to_columns(rows)
-  diagonals = rows_to_left_diagonals(rows) + rows_to_right_diagonals(rows)
-  lines = rows + colums + diagonals
-  # iterate over each length of arbitrary length with each_cons and multiply
+# def rows_to_left_diagonals(rows)
+#   rows = rows.map do |row|
+#     row.reverse()
+#   end
+#   rows_to_right_diagonals(rows)
+# end
+
+# p rows_to_right_diagonals(test)
+# p rows_to_left_diagonals(test)
+
+# def max_product(rows, sequence_length):
+#   columns = rows_to_columns(rows)
+#   diagonals = rows_to_left_diagonals(rows) + rows_to_right_diagonals(rows)
+#   lines = rows + colums + diagonals
+#   # iterate over each length of arbitrary length with each_cons and multiply
+# end
