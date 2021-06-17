d723c0df9e924169e68dd7c2345cb4297ef6f228
Zaxser <nerlenemaedavis@gmail.com>
2021-06-17 13:11:51 +0800

    Solved 9th Project Euler problem (dumb brute force solution)

---
+++ src/euler9.rb
@@ -0,0 +1,19 @@
+# Special Pythagorean triplet
+
+# Problem 9
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a**2 + b**2 = c**2
+# For example, 3**2 + 4**2 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+(0...1000).each do |c|
+  (0...(1000 - c)).each do |b|
+    a = 1000 - c - b
+    if a**2 + b**2 == c**2
+      p a * b * c
+    end
+  end
+end
